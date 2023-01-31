f31c3d5a9b34615b2d7e9d2742a0fbf4ea41b191
Pachecodb <jeferson_luizz@hotmail.com>
2023-01-31 14:31:58 -0300

    atualizaçoes
    feito um modal para excluir itens

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import '././Form.css';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const getListaTarefas = () => {
     let list = localStorage.getItem('lista_tarefas')
@@ -17,6 +19,10 @@ const Form = () => {
     const [tarefa, setTarefa] = useState(getListaTarefas());
     const [toggleSubmit, setToggleSubmit] = useState(true);
     const [isEditItem, setIsEditItem] = useState(null);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     useEffect(() => {
         localStorage.setItem('lista_tarefas', JSON.stringify(tarefa))
@@ -66,35 +72,60 @@ const Form = () => {
     return (
 
         <div className="App">
-                <div>
-                    <h1> Todo-List</h1>
-                    <p>Gerencie sua tarefas!</p>
-                    <form className="was-validated" >
-                        <input type="text" className="form-control" placeholder="Digite a tarefa" value={listaTarefas}
-                            onChange={(e) => setListaTarefas(e.target.value)}  required />
-                        <div className="valid-feedback">Valid.</div>
-                        <div className="invalid-feedback">Por favor insira a tarefa</div>
-                        {
-                            toggleSubmit ? <button type="button" className="btn btn-success" onClick={addTarefa}>Adicionar</button> : <button type="button" className="btn btn-success" onClick={addTarefa}>Adicionar Tarefa Editada</button>
-                        }
-                    </form>
-                </div>
-                <div >
+            <div>
+                <h1> Todo-List</h1>
+                <p>Gerencie sua tarefas!</p>
+                <form className="was-validated" >
+                    <input type="text" className="form-control" placeholder="Digite a tarefa" value={listaTarefas}
+                        onChange={(e) => setListaTarefas(e.target.value)} autofocus required />
+                    <div className="valid-feedback">Valid.</div>
+                    <div className="invalid-feedback">Por favor insira a tarefa</div>
+                    {
+                        toggleSubmit ? <button type="button" className="btn btn-success" onClick={addTarefa}>Adicionar</button> : <button type="button" className="btn btn-primary" onClick={addTarefa}>Salvar Tarefa Editada</button>
+                    }
+                    <Button variant="danger" onClick={handleShow}>
+                        Excluir
+                    </Button>
+                </form>
+            </div>
+            <div >
+                {
+                    tarefa.map((value) => {
+                        return (
+                            <div className="ex1" key={value.id}>
+                                <h3 className="h3">{value.name}</h3>
+                                <div >
+                                    <button type="button" className="btn btn-warning" onClick={() => editTarefa(value.id)}>Editar</button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Excluir Tarefas</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
                     {
                         tarefa.map((value) => {
                             return (
-                                <div className="ex1" key={value.id}>
-                                    <h3 className="h3">{value.name}</h3>
+                                <div key={value.id}>
+                                    <h3 >{value.name}</h3>
                                     <div >
-                                        <button type="button" className="btn btn-danger" onClick={() => deleteTarefa(value.id)}>Excluir</button>
-                                        <button type="button" className="btn btn-warning" onClick={() => editTarefa(value.id)}>Editar</button>
+                                        <button type="button" className="btn btn-danger" onClick={() => deleteTarefa(value.id)} >Excluir</button>
                                     </div>
                                 </div>
                             )
                         })
                     }
-                </div>
-            
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 };
